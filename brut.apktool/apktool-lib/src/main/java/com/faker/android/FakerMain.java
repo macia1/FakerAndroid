3c41a413cd967877bcca193800bf1f8e9d86dced
Efaker <yzg_333@163.com>
2020-11-15 15:47:47 +0800

    Faker 1.0 app test ok

--- brut.apktool/apktool-lib/src/main/java/com/faker/android/FakerMain.java
+++ brut.apktool/apktool-lib/src/main/java/com/faker/android/FakerMain.java
@@ -1,22 +1,6 @@
 package com.faker.android;
-
-import com.googlecode.d2j.dex.Dex2jar;
-import com.googlecode.d2j.reader.BaseDexFileReader;
-import com.googlecode.d2j.reader.MultiDexFileReader;
-import com.googlecode.dex2jar.tools.BaksmaliBaseDexExceptionHandler;
-
-import java.io.File;
-import java.nio.file.Files;
-
 public class FakerMain {
-    ///TODO 来个中文注释吧
-    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\歪歪漫画.apk";
-//    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\牛仔很忙.apk";
-//    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\kwai-android-generic-gifmakerrelease-8.1.10.16599_x32_44c55a.apk";
-
-//    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\牛仔很忙.apk";
-
-//        static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\FakerAndroid-Gradle-dev.apk";
+    static String apkFilePath = "xx.apk";
     public static void main(String[] args) {
         System.out.println(apkFilePath);
         FakerTransfer.translate(apkFilePath,null);
