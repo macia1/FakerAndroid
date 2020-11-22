f881a162086006f1b0164d309861e78c03400dda
Efaker <yzg_333@163.com>
2020-11-22 17:01:12 +0800

    FakerAndroid 1.0.1 fix Collapse Dex To AssetFile

--- brut.apktool/apktool-lib/src/main/java/com/faker/android/FakerMain.java
+++ brut.apktool/apktool-lib/src/main/java/com/faker/android/FakerMain.java
@@ -1,8 +1,6 @@
 package com.faker.android;
 public class FakerMain {
-//    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\大亨联盟_killer.apk";
-//    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\qi_me.apk";
-    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\牛仔很忙.apk";
+    static String apkFilePath = "C:\\xxx.apk";
     public static void main(String[] args) {
         System.out.println(apkFilePath);
         FakerTransfer.translate(apkFilePath,null);
