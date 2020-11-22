79babf92fa67f0ebf6afa4eccd677817a03ddaa4
Efaker <yzg_333@163.com>
2020-11-23 03:07:53 +0800

    FakerAndroid 1.0.1 fix GUI

--- brut.apktool/apktool-lib/src/main/java/com/faker/android/FakerMain.java
+++ brut.apktool/apktool-lib/src/main/java/com/faker/android/FakerMain.java
@@ -1,6 +1,6 @@
 package com.faker.android;
 public class FakerMain {
-    static String apkFilePath = "C:\\Users\\Yang\\Desktop\\apk\\牛仔很忙.apk";
+    static String apkFilePath = "xxx.apk";
     public static void main(String[] args) {
         System.out.println(apkFilePath);
         FakerTransfer.translate(apkFilePath,null,null);
