c895be5fc8a4b2b2e728754158ee74496a82c2a0
Efaker <yzg_333@163.com>
2020-12-06 16:26:32 +0800

    Gui jdk version

--- bin/FakerAndroid-gui/src/main/java/FakerAndroidGui.java
+++ bin/FakerAndroid-gui/src/main/java/FakerAndroidGui.java
@@ -1,8 +1,14 @@
 import faker.android.decoder.util.Jar;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.DragEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.File;
 import java.net.URL;
@@ -10,18 +16,20 @@ import java.net.URL;
 public class FakerAndroidGui extends Application {
 
 	@Override
-	public void start(Stage stage) throws Exception {
-		File file = Jar.getResourceAsFile("/FileChooser.fxml");
-		URL uri = file.toURI().toURL();
-		Parent root = FXMLLoader.load(uri);
+	public void start(Stage primaryStage) throws Exception {
+//		File file = Jar.getResourceAsFile("/FileChooser.fxml");
+//
+//		URL uri = file.toURI().toURL();
+		Parent root = FXMLLoader.load(getClass().getResource("/FileChooser.fxml"));
 		Scene scene = new Scene(root);
-		stage.setTitle("FakerAndroid GUI");
-		stage.setResizable(false);
-		stage.setScene(scene);
-		stage.show();
+		primaryStage.setTitle("FakerAndroid GUI");
+		primaryStage.setResizable(false);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 
 	public static void main(String[] args) {
 		launch(args);
 	}
+
 }
